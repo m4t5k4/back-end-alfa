c473e9b117af4a855c39a66a317bc5c1c3758b3b
bram <bram.vermeulen69@gmail.com>
2021-01-08 16:40:20 +0100

    startbestanden

---
+++ src/main/java/com/example/backendalfa/BackEndAlfaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendalfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAlfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndAlfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendalfa/BackEndAlfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendalfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAlfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
